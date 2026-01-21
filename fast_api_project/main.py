c302b81ad1e0e653afc507182831de41d54a8451
sushant kokate <kokatesushantsk@gmail.com>
2026-01-22 00:59:24 +0530

    employee management system

--- fast_api_project/main.py
+++ fast_api_project/main.py
@@ -3,7 +3,38 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"sushant":"9763280178"}
 
+@app.get("/")
+def root():
+    return {"sushant":"9763"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"this is you post "}
+
+
+@app.delete("/posts/{id}")
+def ok():
+    return {"sushant":"9763280178" }
+
+
+@app.get("/about")
+def about():
+    return {"data":"this is about page"}
+
+
+
+
+@app.get("/contact")
+def contact():
+    return {"data":"this is contact page"}
+
+
+@app.get("/home")
+def home():
+    return {"data":"this is home page"}
+
+
+@app.get("/help")
+def help():
+    return {"data":"this is help page"}
